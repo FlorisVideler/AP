6017ce31b111f8c14ba9848105aed47af8244737
Floris Videler <floris.videler@student.hu.nl>
2020-05-13 15:31:47 +0200

    First test test

--- Formatieve opdracht 2b/test/NodeTest.java
+++ Formatieve opdracht 2b/test/NodeTest.java
@@ -7,12 +7,19 @@ class NodeTest {
 
     @org.junit.jupiter.api.Test
     void addTransition() {
-        Map<Integer, Node> expectedMap = new HashMap<Integer, Node>();
+        Map<Integer, Node> expectedMap = new HashMap<>();
         Node startNode = new Node("Node1", false);
         Node endNode = new Node("Node2", true);
         expectedMap.put(0, endNode);
         startNode.addTransition(0.5f, endNode);
         assertEquals(expectedMap, startNode.transitions);
+    }
+
+    @org.junit.jupiter.api.Test
+    void addEmptyTransition() {
+        Map<Integer, Node> expectedMap = new HashMap<>();
+        Node startNode = new Node("Node1", false);
+        assertThrows( ,startNode.addTransition(0.5f, ););
     }
 
     @org.junit.jupiter.api.Test
