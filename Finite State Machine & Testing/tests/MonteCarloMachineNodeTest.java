6ca2e4426a7bd21bad043458922811c61ae069e6
Floris Videler <floris.videler@student.hu.nl>
2020-05-15 15:06:27 +0200

    Added a MonteCarloMachineNodeTest test

--- Finite State Machine & Testing/tests/MonteCarloMachineNodeTest.java
+++ Finite State Machine & Testing/tests/MonteCarloMachineNodeTest.java
@@ -30,4 +30,17 @@ public class MonteCarloMachineNodeTest {
         //Als de input lager is dan 0.5f dan wordt er gekozen voor node10
         assertEquals(node00.doTransition(0.3f), node10);
     }
+
+    @Test
+    public void doTransitionResultGreaterThanOne() {
+        MonteCarloMachineNode node00 = new MonteCarloMachineNode("Node00");
+
+        MonteCarloMachineNode node10 = new MonteCarloMachineNode("Node10");
+        MonteCarloMachineNode node11 = new MonteCarloMachineNode("Node11");
+
+        node00.addTransition(0.5f, node10);
+        node00.addTransition(0.5f, node11);
+
+        assertEquals(node00.doTransition(1.3f), node10);
+    }
 }
